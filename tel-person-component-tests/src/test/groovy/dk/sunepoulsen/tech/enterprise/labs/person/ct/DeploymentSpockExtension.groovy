db140d86b6b02c2c930cbbeeb33afe6726555257
sunepoulsen <sunepoulsen@gmail.com>
2021-03-21 21:30:03 +0100

    TEL-47: Finalized component tests of POST /persons

--- tel-person-component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/person/ct/DeploymentSpockExtension.groovy
+++ tel-person-component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/person/ct/DeploymentSpockExtension.groovy
@@ -1,16 +1,23 @@
 package dk.sunepoulsen.tech.enterprise.labs.person.ct
 
+import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.db.DbHelper
 import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.spock.DefaultDeploymentSpockExtension
 
 class DeploymentSpockExtension extends DefaultDeploymentSpockExtension {
+    static String COMPOSE_NAME = 'ct'
     static String CONTAINER_NAME = 'tel-person-service'
+    static String DB_CONTAINER_NAME = 'tel-persons-db'
+    static String DB_USER = 'persons'
+    static String DB_PASSWORD = 'jukilo90'
+    static String DB_NAME = 'persons'
 
     DeploymentSpockExtension() {
-        super('ct', [CONTAINER_NAME])
+        super(COMPOSE_NAME, [CONTAINER_NAME])
     }
 
-    @Override
-    void start() {
-        super.start()
+    static void clearDatabase() {
+        DbHelper dbHelper = new DbHelper(deployment, COMPOSE_NAME, DB_CONTAINER_NAME, DB_USER, DB_PASSWORD, DB_NAME)
+
+        dbHelper.clearDatabase(['persons'])
     }
 }
--- tel-person-component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/person/ct/PersonSpec.groovy
+++ tel-person-component-tests/src/test/groovy/dk/sunepoulsen/tech/enterprise/labs/person/ct/PersonSpec.groovy
@@ -3,38 +3,38 @@ package dk.sunepoulsen.tech.enterprise.labs.person.ct
 import com.google.common.net.MediaType
 import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.http.HttpHelper
 import dk.sunepoulsen.tech.enterprise.labs.core.component.tests.verification.HttpResponseVerificator
-import dk.sunepoulsen.tech.enterprise.labs.core.rs.client.utils.JsonUtils
+import dk.sunepoulsen.tech.enterprise.labs.core.rs.client.model.ServiceError
 import dk.sunepoulsen.tech.enterprise.labs.person.rs.client.model.Person
 import dk.sunepoulsen.tech.enterprise.labs.person.rs.client.model.PersonSex
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.net.http.HttpRequest
 import java.time.LocalDate
 
 class PersonSpec extends Specification {
 
-    void "Create new person - OK and created"() {
-        given: 'Person service is available'
-            DeploymentSpockExtension.deployment.waitForAvailable(DeploymentSpockExtension.CONTAINER_NAME)
+    private HttpHelper httpHelper
 
-        and: 'a person body'
+    void setup() {
+        DeploymentSpockExtension.deployment.waitForAvailable()
+        DeploymentSpockExtension.clearDatabase()
+
+        this.httpHelper = new HttpHelper(DeploymentSpockExtension.deployment)
+    }
+
+    void "Create new person - 200"() {
+        given: 'a person body'
             Person newPerson = new Person(
-                    firstName: 'first name',
-                    surnames: 'surnames',
-                    lastSurname: 'last surname',
-                    sex: PersonSex.MALE,
-                    birthDate: LocalDate.now()
+                firstName: 'first name',
+                surnames: 'surnames',
+                lastSurname: 'last surname',
+                sex: PersonSex.MALE,
+                birthDate: LocalDate.now()
             )
 
         when: 'Call POST /persons'
-            HttpHelper httpHelper = new HttpHelper(DeploymentSpockExtension.deployment)
-            HttpRequest httpRequest = httpHelper.newRequestBuilder(DeploymentSpockExtension.CONTAINER_NAME, '/persons')
-                .POST(HttpRequest.BodyPublishers.ofString(JsonUtils.encodeAsJson(newPerson)))
-                .header('Content-Type', MediaType.JSON_UTF_8.toString())
-                .header('X-Request-ID', UUID.randomUUID().toString())
-                .build()
-
-        HttpResponseVerificator verificator = httpHelper.sendRequest(httpRequest)
+            HttpResponseVerificator verificator = httpHelper.createAndSendPostWithJson(DeploymentSpockExtension.CONTAINER_NAME, '/persons', newPerson)
 
         then: 'Response Code is 201'
             verificator.responseCode(201)
@@ -42,14 +42,64 @@ class PersonSpec extends Specification {
         and: 'Response body is json'
             verificator.bodyIsJson()
 
-        and: 'Verify greetings body'
-            verificator.bodyAsJson() == [
-                firstName: newPerson.firstName,
-                surnames: newPerson.surnames,
-                lastSurname: newPerson.lastSurname,
-                sex: newPerson.sex,
-                birthDate: newPerson.birthDate
-            ]
+        and: 'Verify person body'
+            with(verificator.bodyAsJsonOfType(Person)) {
+                id > 0
+                firstName == newPerson.firstName
+                surnames == newPerson.surnames
+                lastSurname == newPerson.lastSurname
+                sex == newPerson.sex
+                birthDate == newPerson.birthDate
+            }
+    }
+
+    void "Create new person - 400 - Person with id"() {
+        given: 'a person body'
+            Person newPerson = new Person(
+                id: 37L,
+                firstName: 'first name',
+                surnames: 'surnames',
+                lastSurname: 'last surname',
+                sex: PersonSex.MALE,
+                birthDate: LocalDate.now()
+            )
+
+        when: 'Call POST /persons'
+            HttpResponseVerificator verificator = httpHelper.createAndSendPostWithJson(DeploymentSpockExtension.CONTAINER_NAME, '/persons', newPerson)
+
+        then: 'Response Code is 400'
+            verificator.responseCode(400)
+
+        and: 'Response body is json'
+            verificator.bodyIsJson()
+
+        and: 'Verify person body'
+            verificator.bodyAsJsonOfType(ServiceError) == new ServiceError(
+                param: 'id',
+                message: 'id must be null'
+            )
+    }
+
+    @Unroll
+    void "Create new person - #_responseCode - #_reason"() {
+        when: 'Call POST /persons'
+            HttpResponseVerificator verificator = httpHelper.createAndSendPostWithBody(DeploymentSpockExtension.CONTAINER_NAME,
+                '/persons',
+                _contentType,
+                HttpRequest.BodyPublishers.ofString(_body)
+            )
+
+        then: 'Response Code is #_responseCode'
+            verificator.responseCode(_responseCode)
+
+        and: 'Response has no body'
+            verificator.noBody()
+
+        where:
+            _contentType                    | _body               | _responseCode | _reason
+            MediaType.JSON_UTF_8.toString() | '{"property":"27,}' | 400           | 'bad json'
+            MediaType.JSON_UTF_8.toString() | '{"sex":"BAD_SEX"}' | 400           | 'valid json with bad enum'
+            MediaType.HTML_UTF_8.toString() | '<html>'            | 415           | 'non json'
     }
 
 }
